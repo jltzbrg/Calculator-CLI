5d29fa06cefb58d99091f32efde222ae7d67a732
Julio Litzenberg <julio.litzenberg@gmail.com>
2020-09-27 15:21:39 +0200

    Fix Typo Fehler

--- Calculator-CLI/IOHelper.swift
+++ Calculator-CLI/IOHelper.swift
@@ -8,3 +8,25 @@
 // 
 
 import Foundation
+
+struct IOHelper {
+    
+    func newInput() -> String? {
+        print("Gib 'h' für Hilfe ein")
+        return ask(":> ")
+    }
+    
+    func ask(_ question: String) -> String? {
+        print("\(question)", terminator: "")
+        return readLine(strippingNewline: true)
+    }
+    
+    func usageInfo() -> String {
+        return "Bitte wählen\n"
+        + "h: Hilfe\n"
+        + "a: Addieren\n"
+        + "s: Subtrahieren\n"
+        + "m: Multipliziren\n"
+        + "d: Dividieren\n"
+    }
+}
