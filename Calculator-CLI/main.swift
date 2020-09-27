2296348cfe7e88bff382b5e588f5a4034930b34e
Julio Litzenberg <julio.litzenberg@gmail.com>
2020-09-27 15:50:45 +0200

    Main.swift - Funktionalität erstellt des gesamten Programm.

--- Calculator-CLI/main.swift
+++ Calculator-CLI/main.swift
@@ -14,4 +14,46 @@ let math = Math()
 
 while true {
     
+    guard let method = iohelper.newInput() else {
+        continue
+    }
+    
+    var mathFunction: (Int, Int) -> Int
+    
+    switch method {
+    case "h":
+        print(iohelper.usageInfo())
+        continue
+    case "a":
+        mathFunction = math.addieren
+    case "s":
+        mathFunction = math.subtrahieren
+    case "m":
+        mathFunction = math.multiplizieeren
+    case "d":
+        mathFunction = math.dividieren
+    case "q":
+        //Programm beenden
+        exit(0)
+    default:
+        print("Eingabe unbekannt")
+        continue
+    }
+    
+    //Eingabe verarbeiten
+    let input1 = iohelper.ask("Zahl Nummer 1: ")
+    let input2 = iohelper.ask("Zahl Nummer 2: ")
+    
+    
+    //Zahl Eins und Zwei validieren
+    guard let str1  = input1, let str2 = input2 else {
+        continue
+    }
+    
+    guard let zahl1 = Int(str1), let zahl2 = Int(str2) else {
+        continue
+    }
+    
+    let result = mathFunction(zahl1, zahl2)
+    print("Ergebnis: \(result)", terminator: "\n\n")
 }
